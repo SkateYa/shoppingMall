0a53980cacdd5b0e30a3736cdcfb85649c3e110d
zhangxinya <1805595661@qqq.com>
2020-12-30 00:21:55 +0800

    新增tabs组件和商品列表页面

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -5,16 +5,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabs: [
+      { id: 0, value: '综合', checked: true },
+      { id: 1, value: '销量', checked: false },
+      { id: 2, value: '价格', checked: false }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(options)
   },
-
+  tabsItemChange(e) {
+    const { index } = e.detail
+    let { tabs } = this.data
+    tabs.forEach((v, i) => i === index ? v.checked = true : v.checked = false)
+    this.setData({
+      tabs
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
